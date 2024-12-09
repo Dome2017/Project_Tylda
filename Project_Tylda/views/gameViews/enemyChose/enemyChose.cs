813438efb4ef8f5edde5081ee6843692e03a6d7a
Dome2017 <dominik.monkos@gmail.com>
2024-12-09 19:43:41 +0100

    Dodana klasa enemy

---
+++ Project_Tylda/views/gameViews/enemyChose/enemyChose.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Tylda.views.gameViews.enemyChose
+{
+    public class enemy
+    {
+        public string Name { get; set; }   
+        public int Hp { get; set; }
+        public int Defense { get; set; }
+        public int Attack { get; set; }
+
+        public enemy(string name, int hp, int defense, int attack)
+        {
+            this.Name = name;
+            this.Hp = hp;
+            this.Defense = defense;
+            this.Attack = attack;
+        }
+    }
+    
+}
