cdca9f460ee3ea86b3b4b85ea4d9ffaec0614fc0
Dome2017 <dominik.monkos@gmail.com>
2024-12-12 20:42:02 +0100

    wyodrębnienie modelu postaci do katalogu Models

---
+++ Project_Tylda/Models/Character.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Tylda
+{
+    public class Character
+    {
+        public string Name { get; set; }
+        public int Hp { get; set; }
+        public int Defense { get; set; }
+        public int Attack { get; set; }
+        public Character(string name, int hp, int defense, int attack)
+        {
+            this.Name = name;
+            this.Hp = hp;
+            this.Defense = defense;
+            this.Attack = attack;
+        }
+    }
+}
--- Project_Tylda/views/characterCreator/characterCreator.cs
+++ Project_Tylda/views/characterCreator/characterCreator.cs
@@ -8,21 +8,6 @@ using System.Xml.Linq;
 
 namespace Project_Tylda
 {
-    public class Character
-    {
-        public string Name { get; set; }  
-        public int Hp { get; set; }
-        public int Defense { get; set; }
-        public int Attack { get; set; }
-
-        public Character(string name, int hp, int defense, int attack)
-        {
-            this.Name = name;
-            this.Hp = hp;
-            this.Defense = defense;
-            this.Attack = attack;
-        }
-    }
     public class CharacterCreator
     {
         public static void CharacterChose()
